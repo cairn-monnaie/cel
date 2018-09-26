820443638cf92f9744d80a7ea028f20535c2fe6e
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2018-09-27 00:32:02 +0200

    Exceptions management in ExceptionListener : if Cyclos error, either catch it(ENTITY_NOT_FOUND), logout(LOGIN error), or declare maintenance state(ConnectionException). Deals with circular errors if, after an exception is detected, the user is redirected to another page throwing exception.

--- src/Cairn/UserBundle/EventListener/ExceptionListener.php
+++ src/Cairn/UserBundle/EventListener/ExceptionListener.php
@@ -51,68 +51,73 @@ class ExceptionListener
         $request = $event->getRequest();
         $session = $request->getSession();
 
-        $previousUrl = $request->headers->get('referer');
         $welcomeUrl = $this->router->generate('cairn_user_welcome');
-        $currentRoute = $request->get('_route');
-        $currentUrl = $request->getPathInfo(); 
+        $logoutUrl = $this->router->generate('fos_user_security_logout');
 
-        $currentRoute = ' Route courante : ' . $currentRoute;
-        $currentUrl = ' URL courante : ' . $currentUrl;
-        $fromUrl = ' Depuis URL : ' .$previousUrl;
+        $currentRoute = ' Route courante : ' . $request->get('_route');
+        $currentUrl = ' URL courante : ' . $currentUrl = $request->getRequestUri(); 
+        $fromUrl = ' Depuis URL : ' .$request->headers->get('referer');
 
         $from = $this->messageNotificator->getNoReplyEmail();
         $to = $this->messageNotificator->getMaintenanceEmail();
 
-        $exceptionMessage = $exception->getMessage();
-        $routesMessage = $currentRoute . $currentUrl . $fromUrl;
+        $exceptionMessage = 'Message d\'erreur : '. $exception->getMessage();
 
-        if($exception instanceof Cyclos\ServiceException){
-            $subject = 'Erreur Cyclos';
+        $traceMessage = 'Trace : '. $exception->getTraceAsString();
+        $lineMessage =  'A la ligne : ' .$exception->getLine();
+        $routesMessage = $currentRoute . "\n" .$currentUrl ."\n". $fromUrl;
 
-            if($exception->errorCode == 'ENTITY_NOT_FOUND'){
-                $session->getFlashBag()->add('error','Donnée introuvable');
-            }
-            else{
-                //not all exceptions have this attribute
-                if(property_exists($exception,'error')){
-                    $error = ' Error : ' . json_encode($exception->error);
+        $body = $routesMessage ."\n". $exceptionMessage . "\n" . $traceMessage;
+
+        //this condition avoids circular exceptions thrown if the redirection page (login / homepage) contains an error
+        if(strpos($request->headers->get('referer'), $request->getRequestUri())){
+            $subject = 'Erreur circulaire';
+            $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
+            $event->setResponse(new RedirectResponse($logoutUrl));
+        }else{
+            if($exception instanceof Cyclos\ServiceException){
+                $subject = 'Erreur Cyclos';
+
+                if($exception->errorCode == 'ENTITY_NOT_FOUND'){
+                    $session->getFlashBag()->add('error','Donnée introuvable');
+                    $event->setResponse(new RedirectResponse($welcomeUrl));
+
                 }
-                $body = $exceptionMessage . $error . $routesMessage;
+                elseif($exception->errorCode == 'LOGIN'){
+                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
+                    $session->getFlashBag()->add('error','Problème de connexion');
+                    $event->setResponse(new RedirectResponse($logoutUrl));
+                }
+                else{
+                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
+                    $session->getFlashBag()->add('error','Une erreur technique est survenue. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.');
+                    $event->setResponse(new RedirectResponse($welcomeUrl));
+                }
+            }
+            elseif($exception instanceof Cyclos\ConnectionException){
+                $subject = 'Maintenance automatique : ConnectionException Cyclos';
+
+                //maintenance state 
+                file_put_contents("../maintenance.txt", '');
+
                 $session->getFlashBag()->add('error','Une erreur technique est survenue. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.');
                 $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
+
+                //will redirect to maintenance page
+                $event->setResponse(new RedirectResponse($welcomeUrl));
             }
-            $event->setResponse(new RedirectResponse($welcomeUrl));
+            else{//not cyclos error, instance of \Exception
+                $subject = 'Erreur technique Symfony';
 
+                if($exception instanceof ContextErrorException){
+                    $context = ' Contexte : ' . json_encode($exception->getContext());
+                    $body = $body . "\n". $context;
+                }
+                $session->getFlashBag()->add('error','Une erreur technique est survenue. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.');
+                $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
+                $event->setResponse(new RedirectResponse($welcomeUrl));
+            }
         }
-        elseif($exception instanceof Cyclos\ConnectionException){
-            $subject = 'Erreur Cyclos';
-            $body = $exceptionMessage . $routesMessage;
-
-            //block all users
-            $this->accessPlatform->shutDown(NULL);
-            $this->em->flush();
-            $session->getFlashBag()->add('error','Une erreur technique est survenue. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.');
-            $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
-
-            $event->setResponse(new RedirectResponse($welcomeUrl));
-
-        }
-//        else{//not cyclos error, instance of \Exception
-//            $subject = 'Erreur technique';
-//            $code = ' Code : ' . $exception->getCode();
-//            $file = ' Fichier : ' . $exception->getFile();
-//            $line = ' Ligne : ' . $exception->getLine();
-//
-//            $trace = ' Trace : ' .$exception->getTraceAsString();
-//            $body = $exceptionMessage . $routesMessage . $code . $file .$line . $trace;
-//            if($exception instanceof ContextErrorException){
-//                $context = ' Contexte : ' . json_encode($exception->getContext());
-//                $body = $body . $context;
-//            }
-//            $session->getFlashBag()->add('error','Une erreur technique est survenue. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.');
-//            $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
-//
-//        }
 
 
     }
