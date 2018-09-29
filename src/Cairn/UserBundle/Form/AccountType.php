6de6f971e9d0d05dbae0e4aabe0a311706f1fd98
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2018-09-29 20:27:16 +0200

    supprimer les espaces entre les nombres pour les ICC

--- src/Cairn/UserBundle/Form/AccountType.php
+++ src/Cairn/UserBundle/Form/AccountType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\AbstractType;
 
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +22,14 @@ class AccountType extends AbstractType
             ->add('email',   EmailType::class, array('label'=>'Email du bénéficiaire',
                                                     'required'=>false))    
             ->add('id',       TextType::class, array('label'=>'ICC',
-                                                     'required'=>false));
+                                                     'required'=>false))
+            ->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+                $account = $event->getData();
+                $form = $event->getForm();
+
+                $account['id'] = preg_replace('/\s/', '', $account['id']);
+                $event->setData($account);
+            });
     }
 
 }
