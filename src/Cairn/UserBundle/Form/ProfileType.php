6d5dfe929f6e6db43b082d4108fbe1d09765d21b
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2018-11-06 13:04:34 +0100

    removed unnecessary data from profile form type : RIB

--- src/Cairn/UserBundle/Form/ProfileType.php
+++ src/Cairn/UserBundle/Form/ProfileType.php
@@ -27,33 +27,8 @@ class ProfileType extends AbstractType
         $builder
                 ->add('description', TextareaType::class)
                 ->add('address' , AddressType::class)
-                ->add('image', ImageType::class,array('required'=>false));
-        $builder->addEventListener(
-            FormEvents::POST_SET_DATA,
-            function (FormEvent $event) {
-                $user = $event->getData();
-                $form = $event->getForm();
-                if(null === $user){
-                    return;
-                }
-
-                if($user->hasRole('ROLE_PRO')){
-                    $form->add('rib', TextType::class, array('label' => 'Votre RIB',
-                        'constraints'=> array(
-                            new Assert\Length(array(
-                                'min'=>23,
-                                'exactMessage'=>'Un RIB est composé de 23 caractères')),
-                            new Assert\Regex(array(
-                                'pattern'=>"#^[0-9]{10}[A-Z0-9]{11}[0-9]{2}$#",
-                                'message'=>'Format invalide'))
-                            )
-                        ));    
-
-                }
-            }
-        );
-
-        $builder->add('save' , SubmitType::class);
+                ->add('image', ImageType::class,array('required'=>false))
+                ->add('save' , SubmitType::class);
     }
 
     /**
