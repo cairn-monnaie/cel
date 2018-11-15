bc9f8efeb70a7fb1c481c9bb7a6a78de501d7bc3
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2018-11-15 16:37:44 +0100

    several scenarios covered with JMeter + assertions on responses. TODO : adapt them with JS and HMTL after web integration is done

--- script_tests_performances.sh
+++ script_tests_performances.sh
@@ -5,6 +5,7 @@ date=$(date +%x)
 
 PATH_ACCOUNT_INFORMATION=tests/performances/AccountsInformation
 PATH_LOGIN=tests/performances/Login
+PATH_ACCESS_MANAGEMENT=tests/performances/AccessManagement
 
 echo "$(tput setaf 3) Start load test for accounts consulting scenario$(tput sgr 0)"
 jmeter -n -l $PATH_ACCOUNT_INFORMATION/$date.$hour.csv -t $PATH_ACCOUNT_INFORMATION/load_test_by_steps.jmx -JnombreUnites-TG1=50 -JdureeMontee-TG1=50 -JnombreUnites-TG2=60 -JdureeMontee-TG2=50
@@ -12,6 +13,10 @@ echo "$(tput setaf 2) Load testing is over ! "
 sleep 5
 
 echo "$(tput setaf 3) Start load test for login scenario$(tput sgr 0)"
-jmeter -n -l $PATH_LOGIN/$date.$hour.csv -t $PATH_LOGIN/load_test_by_steps.jmx -JnombreUnites-TG1=50 -JdureeMontee-TG1=50 -JnombreUnites-TG2=60 -JdureeMontee-TG2=50
+jmeter -n -l $PATH_LOGIN/$date.$hour.csv -t $PATH_LOGIN/load_test_by_steps.jmx 
+echo "$(tput setaf 2) Load testing is over ! "
+
+echo "$(tput setaf 3) Start load test for access management$(tput sgr 0)"
+jmeter -n -l $PATH_ACCESS_MANAGEMENT/$date.$hour.csv -t $PATH_ACCESS_MANAGEMENT/load_test_by_steps.jmx 
 echo "$(tput setaf 2) Load testing is over ! "
 
