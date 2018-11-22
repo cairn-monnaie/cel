7f6ec5e6610e88c761a6b876b99830ea8526bdcc
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2018-11-22 17:48:08 +0100

    create command to install first super admin

--- src/Cairn/UserBundle/Service/Commands.php
+++ src/Cairn/UserBundle/Service/Commands.php
@@ -6,8 +6,16 @@ namespace Cairn\UserBundle\Service;
 use Symfony\Bundle\TwigBundle\TwigEngine;
 use Doctrine\ORM\EntityManager;
 use Cairn\UserBundle\Service\MessageNotificator;
+
+//UserBundle Entities
+use Cairn\UserBundle\Entity\User;
+use Cairn\UserBundle\Entity\Address;
+use Cairn\UserBundle\Entity\Card;
+
 use Cairn\UserCyclosBundle\Entity\UserManager;
+use Cairn\UserCyclosBundle\Entity\AccessClientManager;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\DependencyInjection\Container;
 
 class Commands
 {
@@ -23,7 +31,9 @@ class Commands
 
     protected $router;
 
-    public function __construct(EntityManager $em, MessageNotificator $messageNotificator, TwigEngine $templating, $cardActivationDelay, $emailValidationDelay, Router $router)
+    protected $container;
+
+    public function __construct(EntityManager $em, MessageNotificator $messageNotificator, TwigEngine $templating, $cardActivationDelay, $emailValidationDelay, Router $router, Container $container)
     {
         $this->em = $em;
         $this->messageNotificator = $messageNotificator;
@@ -31,6 +41,95 @@ class Commands
         $this->cardActivationDelay = $cardActivationDelay;
         $this->emailValidationDelay = $emailValidationDelay;
         $this->router = $router;
+        $this->userManager = new UserManager();
+        $this->container = $container;
+    }
+
+    /**
+     *Returns true and creates admin if he does not exist yet, returns false otherwise
+     *
+     *@return boolean 
+     */ 
+    public function createInstallAdmin($username, $password, $activationCode)
+    {
+        $accessClientManager = new AccessClientManager();
+
+        $userRepo = $this->em->getRepository('CairnUserBundle:User');
+        $main_admin = $userRepo->findOneBy(array('username'=>$username));
+        if (!$main_admin){
+
+            //get cyclos reference
+            $credentials = array('username'=>$username,'password'=>$password);
+
+            try{
+                $this->container->get('cairn_user_cyclos_network_info')->switchToNetwork($this->container->getParameter('cyclos_network_cairn'),'login',$credentials);
+            }catch(\Exception $e){
+                if($e->errorCode == 'LOGIN'){
+                    return 'Wrong username or password provided';
+                }
+            }   
+
+            $userVO = $this->container->get('cairn_user_cyclos_user_info')->getUserVOByName($username);
+            $id = $userVO->id;
+            $userData = $this->container->get('cairn_user_cyclos_user_info')->getProfileData($id);
+
+            $new_admin = new User();
+            $new_admin->setUsername($username);
+            $new_admin->setName($userData->name);
+            $new_admin->setEmail($userData->email);
+            $new_admin->setCyclosID($id);
+
+
+            if($this->container->getParameter('kernel.environment') == 'test'){
+                $password = '@@bbccdd';
+            }
+            $new_admin->setPlainPassword($password);
+            $new_admin->setEnabled(true);
+
+            $new_admin->addRole('ROLE_SUPER_ADMIN');
+
+            $zip = $this->em->getRepository('CairnUserBundle:ZipCity')->findOneBy(array('zipCode'=>'38000','city'=>'Grenoble'));
+            $address = new Address();
+            $address->setZipCity($zip);
+            $address->setStreet1('7 rue Très Cloîtres');
+
+            $new_admin->setAddress($address);
+            $new_admin->setDescription('Administrateur de l\'application');
+
+            //set auto-referent
+            $new_admin->addReferent($new_admin);
+
+//            //set client access token for Cyclos web services
+//            $query = new \stdClass();                                              
+//            $query->user = $newUserCyclosID;                                       
+//            $query->type = $this->container->get('cairn_user_cyclos_accessclient_info')->getPrincipalTypeVO();
+//            $dataForNew = $accessClientManager->getDataForNew($query);       
+//
+//                // create a new Access Client                                          
+//            $dto = $dataForNew->dto;                                               
+//            $dto->name = $user->getName().time();                                  
+//            $id = $accessClientManager->save($dto);                          
+//
+//              // get activation code                                                 
+//            $dto = $accessClientManager->load($id);                          
+//            $code = $accessClientManager->getActivationCode($id);            
+//
+            //activate access client                                               
+            $accessClientDTO = $accessClientManager->activateAccessClient($activationCode,'');
+            $new_admin->setCyclosAccessToken($accessClientDTO->token); 
+
+            //ajouter la carte
+            $salt = $this->container->get('cairn_user.security')->generateCardSalt($new_admin);
+            $card = new Card($new_admin,$this->container->getParameter('cairn_card_rows'),$this->container->getParameter('cairn_card_cols'),$salt);
+            $new_admin->setCard($card);
+
+
+            $this->em->persist($new_admin);
+            $this->em->flush();
+
+            return 'admin user has been created successfully !';
+        }
+        return 'admin user has already been created !';
     }
 
     /**
@@ -84,10 +183,9 @@ class Commands
 
                 $params = new \stdClass();                                             
                 $params->status = 'REMOVED';                                           
-                $params->user = $this->get('cairn_user.bridge_symfony')->fromSymfonyToCyclosUser($user);
+                $params->user = $this->container->get('cairn_user.bridge_symfony')->fromSymfonyToCyclosUser($user);
 
-                $userManager = new UserManager();
-                $userManager->changeStatusUser($params);
+                $this->userManager->changeStatusUser($params);
                 $saveEmail = $user->getEmail();
                 $this->em->remove($user);
                 $this->em->flush();
