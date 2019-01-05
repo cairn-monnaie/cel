485105d9a7d5bf1611c5c9a1860da9bae4d6d605
janssens <janssensgaetan@gmail.com>
2019-01-05 09:34:07 +0100

    not used

--- src/Cairn/UserBundle/Resources/views/Pro/crash.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($){
-    $('#consulting').mouseover(function(e) {
-       $(this).append(<a href="{{ path('cairn_user_management_beneficiaries') }}" class="btn btn-default">
-) 
-    });
-});
