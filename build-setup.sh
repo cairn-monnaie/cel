412f37ee3a7c1a63a876ae4eaa45e2a9ab5f40dc
janssens <janssensgaetan@gmail.com>
2019-01-28 22:47:29 +0100

    fresh install fix

--- build-setup.sh
+++ build-setup.sh
@@ -14,10 +14,10 @@ python3.5 setup_cyclos.py $1 `echo -n $2 | base64 `
 echo "Cyclos configuration setup : OK ! "
 
 echo "Creating MySQL database"
-php bin/console doctrine:database:create --env=$1                            
-php bin/console doctrine:migrations:diff --env=$1                       
-php bin/console doctrine:migrations:migrate --env=$1                       
-php bin/console doctrine:database:import --env=$1 web/zipcities.sql          
+php bin/console doctrine:database:drop --env=$1 --force
+php bin/console doctrine:database:create --env=$1
+php bin/console doctrine:schema:create --env=$1
+php bin/console doctrine:database:import --env=$1 web/zipcities.sql
 echo " MySQL database created : OK ! "
 
 echo " Generating administrator user"
