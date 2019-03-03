8b6afa33763207f00d13c946b79cd9e02fe45b45
janssens <janssensgaetan@gmail.com>
2019-03-03 23:45:21 +0100

    jquery in

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Evheniy\JqueryBundle\JqueryBundle(),
             new Cairn\UserCyclosBundle\CairnUserCyclosBundle(),
             new Cairn\UserBundle\CairnUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
