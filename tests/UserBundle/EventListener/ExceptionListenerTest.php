3013905e5f46f8d1fcf84189f004b98011141543
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2019-03-03 13:09:50 +0100

    edited cyclos exception messages + adapt exception listener tests + no more email sent in case the user is logged_out from cyclos before symfony session

--- tests/UserBundle/EventListener/ExceptionListenerTest.php
+++ tests/UserBundle/EventListener/ExceptionListenerTest.php
@@ -92,7 +92,8 @@ class ExceptionListenerTest extends KernelTestCase
         $this->assertTrue($event->getResponse()->isRedirect($logout));
 
         $messages_flash = $event->getRequest()->getSession()->getFlashBag()->get('error',array());
-        $this->assertContains('Problème de connexion',$messages_flash[0]);
+        $this->assertContains('problème technique',$messages_flash[0]);
+        $this->assertContains('phase de connexion',$messages_flash[0]);
 
 
         //test ENTITY_NOT_FOUND
@@ -141,7 +142,7 @@ class ExceptionListenerTest extends KernelTestCase
         $this->assertTrue($event->getResponse()->isRedirect($welcome));
 
         $messages_flash = $event->getRequest()->getSession()->getFlashBag()->get('error',array());
-        $this->assertContains('erreur technique',$messages_flash[0]);
+        $this->assertContains('problème technique',$messages_flash[0]);
 
         //test ConnectionException
         $exception = new Cyclos\ConnectionException();
