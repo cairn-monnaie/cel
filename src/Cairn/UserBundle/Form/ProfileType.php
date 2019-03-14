139d6ff271c4c8e7fa0f28b81ce5cadac23c7b81
janssens <janssensgaetan@gmail.com>
2019-03-14 15:10:40 +0100

    profil edit

--- src/Cairn/UserBundle/Form/ProfileType.php
+++ src/Cairn/UserBundle/Form/ProfileType.php
@@ -26,14 +26,14 @@ class ProfileType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->remove('username');
+        $builder->add('username',TextType::class,array('label'=>'Nom d\'utilisateur','disabled'=>true));
         $builder
             ->add('current_password', PasswordType::class,array(
                 'label'=>'Mot de passe actuel',
                 'mapped'=>false,
                 'constraints'=>new UserPassword() ))
-            ->add('description', TextareaType::class)
-            ->add('address' , AddressType::class)
-            ->add('image', ImageType::class);
+            ->add('address' , AddressType::class);
         $builder->addEventListener(
             FormEvents::SUBMIT,
             function (FormEvent $event) {
@@ -45,7 +45,41 @@ class ProfileType extends AbstractType
                 $user->setPlainPassword($form->get('current_password')->getData());
             }
         );
-        $builder->add('save' , SubmitType::class);
+        $builder->addEventListener(
+            FormEvents::POST_SET_DATA,
+            function (FormEvent $event) {
+                $user = $event->getData();
+                $form = $event->getForm();
+                if(null === $user){
+                    return;
+                }
+                if($user->hasRole('ROLE_PRO')){
+                    $form->add('name', TextType::class,array('label'=>'Nom de la structure'));
+                    //$form->add('image', ImageType::class,array('label'=>'Logo'));
+                    $form->add('description',TextareaType::class,array('label'=>'Décrivez ici votre activité en quelques mots ...'));
+                    if ($user->getImage())
+                        $form->add('image', ImageType::class,array('label'=>'Changer votre logo'));
+                    else
+                        $form->add('image', ImageType::class,array('label'=>'Votre logo'));
+
+                }elseif($user->hasRole('ROLE_PERSON')){
+                    $form->add('name', TextType::class,array('label'=>'Votre nom'));
+                    $form->add('firstname', TextType::class,array('label'=>'Votre prénom'));
+                    $form->add('description',TextareaType::class,array('label'=>
+                        'Décrivez ici en quelques mots pourquoi vous utilisez le Cairn :) '));
+                    if ($user->getImage())
+                        $form->add('image', ImageType::class,array('label'=>'Remplacer votre photo'));
+                    else
+                        $form->add('image', ImageType::class,array('label'=>'Votre photo'));
+                }else{
+                    $form->add('name', TextType::class,array('label'=>'Nom de la structure admin'));
+                    $form->add('description',TextareaType::class,array('label'=>
+                        'Décrivez ici en quelques mots son rôle au sein du Cairn :) '));
+
+                }
+            }
+        );
+        //$builder->add('enregistre' , SubmitType::class);
     }
 
     /**
