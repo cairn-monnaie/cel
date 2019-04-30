9a8350ab05e8eea233ad388533abe24cdd424e92
Mazouth--Laurol Maxime <maxime.mazouth--laurol@grenoble-inp.org>
2019-04-30 15:22:55 +0200

    SmsDataType as a collection

--- src/Cairn/UserBundle/Form/SmsDataType.php
+++ src/Cairn/UserBundle/Form/SmsDataType.php
@@ -2,9 +2,8 @@
 
 namespace Cairn\UserBundle\Form;
 
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use OneSmsDataType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 
@@ -31,43 +30,12 @@ class SmsDataType extends AbstractType
     {
 
         $builder
-            ->add('phoneNumber', TextType::class, array('label'=>'Numéro de téléphone portable'))
-            ->add('smsEnabled',  CheckboxType::class, array('label'=>'Autoriser les opérations par SMS',
-                                                              'required'=>false));
-
-//           ->add('dailyAmountThreshold', IntegerType::class, array('label'=>'Montant max/jour en SMS sans validation',
-//                       'constraints'=> new Assert\Range(array('min'=> 0,'max'=>50,
-//                                                 'minMessage' => 'Un nombre négatif n\'a pas de sens !',
-//                                                 'maxMessage' => 'Au dessus de 50, la carte de sécurité est obligatoire'))
-//           ))
-//           ->add('dailyNumberPaymentsThreshold'  , IntegerType::class ,array('label' => 'Nombre de paiements SMS / jour sans validation',
-//                       'constraints'=> new Assert\Range(array('min'=> 0,'max'=>5,
-//                                                 'minMessage' => 'Un nombre négatif n\'a pas de sens !',
-//                                                 'maxMessage' => 'Au dessus de 5 paiements dans la même journée, la carte de sécurité est obligatoire'))
-//           ))
-        $builder->addEventListener(
-            FormEvents::POST_SET_DATA,
-            function (FormEvent $event) {
-                $smsData = $event->getData();
-                $form = $event->getForm();
-                if(null === $smsData){
-                    return;
-                }
-
-                if($smsData->getUser()->hasRole('ROLE_PRO')){
-                    $form->add('paymentEnabled',CheckboxType::class, array('label'=>'Autoriser la réalisation de paiements par SMS depuis ce numéro','required'=>false))
-                         ->add('smsEnabled',  CheckboxType::class, array('label'=>'Autoriser la réception de paiements par SMS',
-                                                              'required'=>false));
-
-                    if($this->authorizationChecker->isGranted('ROLE_ADMIN')){
-                        $form->add('identifier', TextType::class, array('label' => 'ID SMS'))
-                            ->remove('phoneNumber');
-                    }
-
-                }
-            });
-
-            $builder->add('save', SubmitType::class, array('label' => 'Suivant'));
+            ->add('phoneNumber', CollectionType::class, array(
+                'entry_type' => OneSmsDataType::class,
+                'allow_add' => true,
+                'allow_delete' => true
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Suivant'));
     }
 
     /**
