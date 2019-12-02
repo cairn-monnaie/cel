3f4c1b53aa887382cd90b3807f7e1773163eadf0
Maxime Mazouth-Laurol <maxime.mazouth-laurol@cairn-monnaie.com>
2019-12-02 16:38:47 +0100

    if there is a paymentID in the post request, then there is a transfer to synchronize. Otherwise, it a failed payment to notify an user about

--- src/Cairn/UserBundle/Controller/SecurityController.php
+++ src/Cairn/UserBundle/Controller/SecurityController.php
@@ -179,133 +179,126 @@ class SecurityController extends Controller
             $credentials = array('username'=>$anonymous,'password'=>$anonymous);
             $networkInfo->switchToNetwork($networkName,'login', $credentials);
 
-            //first, we check that the provided paymentID matches an operation in Cyclos
-            //for now, this is a 'transfer' because we do not deal with scheduled conversions. It means that, in Cyclos,
-            //any transaction has its associated transfer
-            $cyclosTransfer = $this->get('cairn_user_cyclos_banking_info')->getTransferByID($data['paymentID']);
+            if(array_key_exists('paymentID',$data)){ // there is a transfer to synchronize
+                $cyclosTransfer = $this->get('cairn_user_cyclos_banking_info')->getTransferByID($data['paymentID']);
 
-            //the validation process already ensures that such a transaction does not already exist in Symfony because the attribute
-            //paymentID is unique. But we give another try on the paymentID of the returned transaction, just in case it is different
-            $res = $em->getRepository('CairnUserBundle:Operation')->findOneBy(array('paymentID'=>$cyclosTransfer->id));
-            if($res){
-                throw new SuspiciousOperationException('Payment already registered');
-            }
+                //the validation process already ensures that such a transaction does not already exist in Symfony because the attribute
+                //paymentID is unique. But we give another try on the paymentID of the returned transaction, just in case it is different
+                $res = $em->getRepository('CairnUserBundle:Operation')->findOneBy(array('paymentID'=>$cyclosTransfer->id));
+                if($res){
+                    throw new SuspiciousOperationException('Payment already registered');
+                }
 
-            //Finally, we check that cyclos transfer data correspond to the POST request
-            $amount = ($data['amount'] == $cyclosTransfer->currencyAmount->amount);
-            $fromAccountNumber = ($data['fromAccountNumber'] == $cyclosTransfer->from->number);
-            $toAccountNumber = ($data['toAccountNumber'] == $cyclosTransfer->to->number);
+                //Finally, we check that cyclos transfer data correspond to the POST request
+                $amount = ($data['amount'] == $cyclosTransfer->currencyAmount->amount);
+                $fromAccountNumber = ($data['fromAccountNumber'] == $cyclosTransfer->from->number);
+                $toAccountNumber = ($data['toAccountNumber'] == $cyclosTransfer->to->number);
 
-            if($amount && $fromAccountNumber && $toAccountNumber){
-                $operation = new Operation();
-                $operation->setDebitorName($this->get('cairn_user_cyclos_user_info')->getOwnerName($cyclosTransfer->from->owner));
-                $operation->setCreditorName($this->get('cairn_user_cyclos_user_info')->getOwnerName($cyclosTransfer->to->owner));
+                if($amount && $fromAccountNumber && $toAccountNumber){
+                    $operation = new Operation();
+                    $operation->setDebitorName($this->get('cairn_user_cyclos_user_info')->getOwnerName($cyclosTransfer->from->owner));
+                    $operation->setCreditorName($this->get('cairn_user_cyclos_user_info')->getOwnerName($cyclosTransfer->to->owner));
 
-                switch ($type){
-                case "conversion":
-                    $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByName($operation->getCreditorName()));
-                    $operation->setType(Operation::TYPE_CONVERSION_BDC);
-                    break;
-                case "deposit":
-                    $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByName($operation->getCreditorName()));
-                    $operation->setType(Operation::TYPE_DEPOSIT);
-                    break;
-                case "withdrawal":
-                    $operation->setDebitor($em->getRepository('CairnUserBundle:User')->findOneByName($operation->getDebitorName()));
-                    $operation->setType(Operation::TYPE_WITHDRAWAL);
-                    break;
-                case "recurring":
-                    $recurringPaymentData = $this->get('cairn_user_cyclos_banking_info')->getRecurringTransactionDataByID($data['transactionID']);
+                    switch ($type){
+                    case "conversion":
+                        $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByMainICC($data['toAccountNumber']));
+                        $operation->setType(Operation::TYPE_CONVERSION_BDC);
+                        break;
+                    case "deposit":
+                        $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByMainICC($data['toAccountNumber']));
+                        $operation->setType(Operation::TYPE_DEPOSIT);
+                        break;
+                    case "withdrawal":
+                        $operation->setDebitor($em->getRepository('CairnUserBundle:User')->findOneByMainICC($data['fromAccountNumber']));
+                        $operation->setType(Operation::TYPE_WITHDRAWAL);
+                        break;
+                    case "recurring":
+                        $recurringPaymentData = $this->get('cairn_user_cyclos_banking_info')->getRecurringTransactionDataByID($data['transactionID']);
 
-                    $operation->setDebitor($em->getRepository('CairnUserBundle:User')->findOneByName($operation->getDebitorName()));
-                    $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByName($operation->getCreditorName()));
+                        $operation->setDebitor($em->getRepository('CairnUserBundle:User')->findOneByMainICC($data['fromAccountNumber']));
+                        $operation->setCreditor($em->getRepository('CairnUserBundle:User')->findOneByMainICC($data['toAccountNumber']));
 
-                    $operation->setExecutionDate(new \Datetime($cyclosTransfer->date));
-                    $operation->setSubmissionDate(new \Datetime($recurringPaymentData->transaction->date));
+                        $operation->setExecutionDate(new \Datetime($cyclosTransfer->date));
+                        $operation->setSubmissionDate(new \Datetime($recurringPaymentData->transaction->date));
 
 
-                    $operation->setRecurringID($data['transactionID']);
-                    if($data['status'] == 'FAILED'){
-                        //send email to debitor user
-                        $body = $this->get('templating')->render('CairnUserBundle:Emails:failed_transaction.html.twig',
-                            array('operation'=>$operation));
-
-                        $subject = 'Echec de votre virement programmé';
-                        $from = $messageNotificator->getNoReplyEmail();
-                        $to = $operation->getDebitor()->getEmail();
-                        $messageNotificator->notifyByEmail($subject,$from,$to,$body);
-
-                        $response = new Response('Transfer Failed. Email sent to user');
-                        $response->setStatusCode(Response::HTTP_OK);
-                        $response->headers->set('Content-Type', 'application/json'); 
-                        $response->headers->set('Accept', 'application/json'); 
-
-                        return $response;
-                    }else{
+                        $operation->setRecurringID($data['transactionID']);
                         $operation->setType(Operation::TYPE_TRANSACTION_EXECUTED);
-                    }
-                    break;
-                case "scheduled":
-                    $existingOperation = $em->getRepository('CairnUserBundle:Operation')->findOneBy(array('paymentID'=>$data['transactionID'],'type'=>Operation::TYPE_TRANSACTION_SCHEDULED));
 
-                    if($existingOperation){
-                        $operation = $existingOperation;
-                    }
+                        break;
+                    case "scheduled":
+                        $existingOperation = $em->getRepository('CairnUserBundle:Operation')->findOneBy(array('paymentID'=>$data['transactionID'],'type'=>Operation::TYPE_TRANSACTION_SCHEDULED));
 
-                                        
-                    if($data['status'] == 'FAILED'){
-                        //send email to debitor user
-                        $body = $this->get('templating')->render('CairnUserBundle:Emails:failed_transaction.html.twig',
-                            array('operation'=>$operation));
+                        if($existingOperation){
+                            $operation = $existingOperation;
+                        }
 
-                        $subject = 'Echec de votre virement programmé';
-                        $from = $messageNotificator->getNoReplyEmail();
-                        $to = $operation->getDebitor()->getEmail();
-                        $messageNotificator->notifyByEmail($subject,$from,$to,$body);
-
-                        $response = new Response('Transfer Failed. Email sent to user');
-                        $response->setStatusCode(Response::HTTP_OK);
-                        $response->headers->set('Content-Type', 'application/json'); 
-                        $response->headers->set('Accept', 'application/json'); 
-
-                        return $response;
-                    }else{
                         $operation->setExecutionDate(new \Datetime($cyclosTransfer->date));
                         $operation->setType(Operation::TYPE_TRANSACTION_EXECUTED);
-                    }
-                    break;
+                        break;
 
-                default:
+                    default:
+                        throw new SuspiciousOperationException('Unexpected operation type');
+                    }
+
+                    $operation->setPaymentID($data['paymentID']);
+                    $operation->setFromAccountNumber($data['fromAccountNumber']);
+                    $operation->setToAccountNumber($data['toAccountNumber']);
+                    $operation->setAmount($data['amount']);
+
+                    //there is not 'reason' property in Cyclos. Then, we use the only one available (description) to set up the 
+                    //operation reason on Symfony side
+                    $operation->setReason($data['description']);
+
+
+                    $em->persist($operation);
+                    $em->flush();
+                    $response = new Response(' { "message"=>"Operation synchronized !" }');
+
+                    $response->setStatusCode(Response::HTTP_CREATED);
+                    $response->headers->set('Content-Type', 'application/json'); 
+                    $response->headers->set('Accept', 'application/json'); 
+
+                    return $response;
+                }
+            }else{ //there is a failed payment to notify
+
+                $cyclosTransaction = $this->get('cairn_user_cyclos_banking_info')->getTransactionDataByID($data['transactionID'])->transaction;
+
+                $debitor = $em->getRepository('CairnUserBundle:User')->findOneByUsername($cyclosTransaction->fromOwner->shortDisplay);
+                if( ($type == "recurring") | ($type == "scheduled")){
+                    if($data['status'] == 'FAILED'){
+                        //send email to debitor user
+                        $body = $this->get('templating')->render('CairnUserBundle:Emails:failed_transaction.html.twig',
+                            array('cyclosTransaction'=>$cyclosTransaction));
+
+                        $subject = 'Echec de votre virement programmé';
+                        $from = $messageNotificator->getNoReplyEmail();
+                        $to = $debitor->getEmail();
+                        $messageNotificator->notifyByEmail($subject,$from,$to,$body);
+
+                        $response = new Response(' { "message"=>"Notification about failed payment sent !" }');
+
+                        $response->setStatusCode(Response::HTTP_OK);
+
+                    }else{
+                        $response = new Response(' { "message"=>"Nothing to send !" }');
+                        $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+                    }
+                }else{
                     throw new SuspiciousOperationException('Unexpected operation type');
                 }
 
-                $operation->setPaymentID($data['paymentID']);
-                $operation->setFromAccountNumber($data['fromAccountNumber']);
-                $operation->setToAccountNumber($data['toAccountNumber']);
-                $operation->setAmount($data['amount']);
-
-                //there is not 'reason' property in Cyclos. Then, we use the only one available (description) to set up the 
-                //operation reason on Symfony side
-                $operation->setReason($data['description']);
-                
-
-                $em->persist($operation);
-                $em->flush();
-                $response = new Response('Operation synchronized');
-                $response->setStatusCode(Response::HTTP_CREATED);
                 $response->headers->set('Content-Type', 'application/json'); 
                 $response->headers->set('Accept', 'application/json'); 
 
                 return $response;
-            }else{
-                $response = new Response('Synchronization failed');
-                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
-                $response->headers->set('Content-Type', 'application/json'); 
-
-                return $response;
             }
+
+
+
         }else{
-            $response = new Response('POST method accepted !');
+            $response = new Response(' { "message"=>"POST method accepted !" }');
             $response->setStatusCode(Response::HTTP_METHOD_NOT_ALLOWED);
             $response->headers->set('Content-Type', 'application/json'); 
 
