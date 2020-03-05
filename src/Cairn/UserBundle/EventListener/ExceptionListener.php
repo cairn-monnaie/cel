0256e636f017a16a9285647fbf8d72ddd8f6d37a
Maxime Mazouth-Laurol <maxime.mazouth-laurol@cairn-monnaie.com>
2020-03-05 17:09:23 +0100

    no more email

--- src/Cairn/UserBundle/EventListener/ExceptionListener.php
+++ src/Cairn/UserBundle/EventListener/ExceptionListener.php
@@ -121,13 +121,10 @@ class ExceptionListener
                     $this->sendException($event, $errorMessage, $welcomeUrl);
                 }
                 elseif($exception->errorCode == 'LOGIN'){
-                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
-
                     $errorMessage = 'Un problème technique est apparu pendant la phase de connexion. Notre service technique en a été automatiquement informé.';
                     $this->sendException($event, $errorMessage, $logoutUrl);
                 }
                 elseif($exception->errorCode == 'PERMISSION_DENIED'){
-                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
                     $errorMessage = 'Vous n\'avez pas les droits nécessaires';
                     $this->sendException($event, $errorMessage, $welcomeUrl);
                 }
@@ -139,7 +136,6 @@ class ExceptionListener
                     $event->setResponse(new RedirectResponse($logoutUrl));
                 }
                 elseif($exception->errorCode == 'NULL_POINTER'){
-                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
                     $errorMessage = 'Donnée introuvable';
                     $this->sendException($event, $errorMessage, $welcomeUrl);
                 }
@@ -155,8 +151,6 @@ class ExceptionListener
                     $this->sendException($event, $errorMessage, $welcomeUrl);
                 }
                 else{
-                    $this->messageNotificator->notifyByEmail($subject,$from,$to,$body);
-
                     $errorMessage = 'Un problème technique est survenu. Notre service technique en a été informé et traitera le problème dans les plus brefs délais.';
                     $this->sendException($event, $errorMessage, $welcomeUrl);
                 }
